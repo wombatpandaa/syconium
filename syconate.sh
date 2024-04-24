b0fea454b72d6abbe31067888927efafd6a2c074
Chad <53791319+wombatpandaa@users.noreply.github.com>
2024-04-24 11:39:56 -0400

    Adding to syconate.sh, editing down, modifying fig files, delete unnecessary cat file and test files

--- syconate.sh
+++ syconate.sh
@@ -4,35 +4,51 @@
 
 echo "This is a script that executes according to the fig it is fed."
 
-# Pre-scans for any figs
+# Scans for figs in current directory
 echo "Scanning for figs..."
-mapfile -t scanned_figs < <(find . -maxdepth 1 -type f -name "*.fig" -not -name ".*")
+mapfile -t scanned_figs < <(find . -type f -name "*.fig")
 if [[ -f ${scanned_figs[0]} ]]; then
-  echo "Fig found!"
+  echo "Found!"
+else
+  echo "No figs found, add a fig to the same directory as syconate.sh"
 fi
 
-if [[ ${#scanned_figs[0]} -gt 1 ]]; then
+# If more than one fig, tell user it defaults to the first
+if [[ $(find . -type f -name "*.fig" | wc -l) -gt 1 ]]; then
   echo "Multiple figs scanned. Defaulting to first fig ${scanned_figs[0]}"
-  default_fig=${scanned_figs[0]}
+else
+  echo "Using ${scanned_figs[0]}"
 fi
 
-# Prompts the user for a fig
-current_fig=$(read -p "Which fig do you wish to use? Press enter to use $default_fig")
+# Set current fig to be the first scanned
+current_fig=${scanned_figs[0]}
 
-# Checks user input
-if [[ ! -f $current_fig ]]; then
-  # If no current exists, check if default is valid file
-  if [[ ! -f $default_fig ]]; then
-    echo "No fig entered and no default fig found. Restart the script and either specify a fig or put a .fig file in the same directory"
-    exit 1
-  # If default exists, use it as current
+#echo "Reached end of file with $current_fig in memory"
+
+# Import current fig for variable usage
+#shellcheck source=. #This is only used by shellcheck
+source $current_fig
+
+# Creates a user if the current fig asks for it
+if [[ $username && $pswd ]]; then
+  echo "Adding user $username with dummy password"
+  sudo useradd $username
+  sudo id $username
+  echo "$username:$pswd" | sudo chpasswd
+  sudo chage -l $username
+fi
+
+# Modifies access control list for a user if the current fig asks for it
+if [[ $hideuser && $hidedir ]]; then
+  echo "Making a hidden directory $hidedir for $hideuser"
+  if [[ $hidedir ]]; then
+    rmdir $hidedir
   fi
-  echo "Using $default_fig."
-  current_fig=$default_fig
-elif [[ ! -f $current_fig ]]; then  # If no default and current is garbage
-  echo "No fig selected. Restart the script and input a fig."
-  exit 1
-else                                # If no default and current is not garbage, use current
-  echo "Using $current_fig."
+  mkdir $hidedir
+  sudo chown $hideuser $hidedir
+  sudo chmod 700 $hidedir
+  setfacl -m g::--- $hidedir
+  setfacl -m o::--- $hidedir
+  setfacl -m u:$hideuser:rwx $hidedir
+  getfacl $hidedir
 fi
-echo "Reached end of file with $current_fig in memory"
