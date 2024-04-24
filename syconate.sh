234c592c14eb0b76753345db455ab5bca75794e8
Chad <53791319+wombatpandaa@users.noreply.github.com>
2024-04-24 09:57:39 -0400

    Numerous updates to syconate script

--- syconate.sh
+++ syconate.sh
@@ -1,20 +1,26 @@
+#!/bin/#!/usr/bin/env bash
+
 # This is the script which executes according to the fig it is fed. Edit a fig or create a new fig to modify how this script runs.
 
 echo "This is a script that executes according to the fig it is fed."
 
 # Pre-scans for any figs
 echo "Scanning for figs..."
-find . -type f -name "*.fig" | declare default_fig # -maxdepth 1
-echo "$default_fig"
-if [[ ! -z $default_fig ]]; then
+mapfile -t scanned_figs < <(find . -maxdepth 1 -type f -name "*.fig" -not -name ".*")
+if [[ -f ${scanned_figs[0]} ]]; then
   echo "Fig found!"
 fi
 
+if [[ ${#scanned_figs[0]} -gt 1 ]]; then
+  echo "Multiple figs scanned. Defaulting to first fig ${scanned_figs[0]}"
+  default_fig=${scanned_figs[0]}
+fi
+
 # Prompts the user for a fig
-declare current_fig = $(read -p "Which fig do you wish to use? Press enter to use $default_fig or type in a different fig name like so: example.fig")
+current_fig=$(read -p "Which fig do you wish to use? Press enter to use $default_fig")
 
 # Checks user input
-if [[ -z $current_fig ]]; then
+if [[ ! -f $current_fig ]]; then
   # If no current exists, check if default is valid file
   if [[ ! -f $default_fig ]]; then
     echo "No fig entered and no default fig found. Restart the script and either specify a fig or put a .fig file in the same directory"
@@ -22,9 +28,11 @@ if [[ -z $current_fig ]]; then
   # If default exists, use it as current
   fi
   echo "Using $default_fig."
-  $current_fig = $default_fig
+  current_fig=$default_fig
 elif [[ ! -f $current_fig ]]; then  # If no default and current is garbage
   echo "No fig selected. Restart the script and input a fig."
   exit 1
 else                                # If no default and current is not garbage, use current
   echo "Using $current_fig."
+fi
+echo "Reached end of file with $current_fig in memory"
